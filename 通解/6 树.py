11322d61fbc90d37b179d5d32ea7fac2065be4e5
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-02-15 23:06:07 +0800

    Update 6 树.py

--- 通解/6 树.py
+++ 通解/6 树.py
@@ -25,4 +25,67 @@ def func(root):
   return abs(h(root.left)-h(root.right))<2 and func(root.left) and func(root.right)
 
 
-5 
+5 路径之和为定值
+def func(root):
+  if not root:
+    return False
+  queue1=Collections.deque([root])
+  queue2=Collections.deque([root.val])
+  while 
+  
+def func(root,sum):
+  if not root:
+    return false
+  if not root.left and root.right:
+    return root.val==sum
+  return func(root.left,sum-root.val) or func(root.right,sum-root.right)
+6 翻转树
+def func(root):
+  if not root:
+    return 0
+  queue=[root]
+  while queue:
+    r=queue.pop(0)
+    r.left,r.right=r.right,r.left
+    if r.left:
+      queue.append(r.left)
+    if r.right:
+      queue.qppend(r.right)
+  return root
+
+def func(root):
+  if not root:
+    return 0
+  root.left,root.right=root.right,root.left
+  func(root.left)
+  func(root.right)
+  return root
+
+7 所有路径
+BFS:
+def func(root):
+  if not root:
+    return []
+  l=list()
+  quenode=Collections.deque([root])
+  queval=Collections.deque([root.val])
+  while queue:
+    r=quenode.popleft()
+    rv=queval.popleft()
+    if not r.left and not r.right:
+      l.append(queval)
+    if r.left:
+      quenode.append(r.left)
+      queval.append(rv+'->'+str(r.left.val))
+    if r.right:
+      quenode.append(r.right)
+      queval.append(rv+'->'+str(r.right.val))
+      
+  return queval
+    
+      
+    
+    
+    
+
+                         
