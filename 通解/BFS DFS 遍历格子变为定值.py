e6201195d0cc4106cc9ba0d641c6b6196771bd30
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-02-17 06:30:17 +0800

    Update BFS DFS 遍历格子变为定值.py

--- 通解/BFS DFS 遍历格子变为定值.py
+++ 通解/BFS DFS 遍历格子变为定值.py
@@ -5,15 +5,17 @@ DFS:
       if 0<i+x<=len(grid) and 0<j+y<=len(grid[0]) and grid[][]=='0':
         dfs(i+x,j+y)
         
- BFS:
+ BFS:####注意需要visited记录已经遍历过的格子
   def bfs(x,y):
     queue=deque([x,y])
+    visited={(x,y)}
     while queue:
       x,y=queue.popleft()
       gird[x][y]='a'
       for i,j in [().()...]:
-        if 0<x+i<=len(grid) and  0<j+y<=len(grid[0]) and grid[][]=='0':
+        if 0<x+i<=len(grid) and  0<j+y<=len(grid[0]) and grid[][]=='0'and () not in visited:
           queue.append((x+i,j+y))
+          visited.add(())
       
       
       
