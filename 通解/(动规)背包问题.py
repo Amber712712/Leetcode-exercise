562d4f4e7e11f479bacfb383d7aaa90482fbf19c
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-03-02 07:05:44 +0800

    Update and rename 背包问题.py to (动规)背包问题.py

--- 通解/(动规)背包问题.py
+++ 通解/(动规)背包问题.py
@@ -1,3 +1,4 @@
+0-1背包
 1 分为等子集
 ##注意边界值 dp[...][0]=true  dp[0][...]=false 放或不放
 
@@ -23,7 +24,7 @@ def func(nums):
       else:
         dp[i][j]=dp[i-1][j] or dp[i-1][j-nums[i-1]]
         
-2 背包问题； 体积固定 值最大 T为体积
+2 背包问题； 体积固定 值最大 T为体积  ###放或不放
 def func(nums,val,t):
   n=len(nums)
   dp=[[0]*(t+1) for i in range(n+1)]
@@ -36,7 +37,23 @@ def func(nums,val,t):
         dp[i][j]=max(dp[i-1][j],dp[i-1][j-nums[i-1]]+val[i-1])
         
  return dp[-1][-1]
-3 零钱问题1（最少零钱数）
+完全背包问题  可用硬币的类型有无限个
+3 零钱兑换  写一个函数来计算可以凑成总金额的硬币组合数
+def func(nums,t):
+  n=len(nums)
+  dp=[[0]*(t+1) for i in range(n+1)]
+  for i in range(n+1):
+    dp[i][0]=1
+  for i in range(1,n+1):
+    for j in range(1,t+1):
+      if j<nums[i-1]:
+        dp[i][j]=dp[i-1][j]
+      else:
+        dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]]
+        
+  return dp[-1][-1]
+
+
 
 
       
