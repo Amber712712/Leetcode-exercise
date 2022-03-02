c4ac5968af569ec21e16de9389ee9d98da959e34
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-03-02 09:31:51 +0800

    Update 6 树.py

--- 通解/6 树.py
+++ 通解/6 树.py
@@ -1,6 +1,98 @@
+dfs: root.left+[]+root.right
+  root.left->root.left.left+[]+root.left.right
 1 前中后序：dfs stack迭代
-2 最大最小高度
-3 相同树 对称树
+https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/python3-er-cha-shu-suo-you-bian-li-mo-ban-ji-zhi-s/
+中序：
+dfs:
+  def func(root):
+    if not root:
+      return []
+    return [root.val]+dfs(root.left)+dfs(root.right)
+前序：
+def func(root):
+  if not root:
+    return []
+  return dfs(root.left)+[root.val]+dfs(root.right)
+
+
+2 最大最小高度 (dfs和bfs)
+最大高度：
+def func(root):
+  if not root:
+    return 0
+  return max(func(root.left),func(root.right))+1
+
+def func(root):
+  if not root:
+    return 0
+  queue=[root]
+  step=0
+  while queue:
+    
+    for i in range(len(queue)):
+      r=queue.pop(0)
+      if not root.left:
+        queue.append(root.left)
+      if not root.right:
+        queue.append(root.right)
+    step+=1
+    
+ return step
+      
+
+3 相同树 (dfs bfs)
+dfs 
+def func(root1,root2):
+  if not root1 and not root2:
+    return True
+  if not root1 or not root2:
+    return False
+  if root1.val!=root2.val:
+    return False
+  
+  return func(root1.left,root1.left) and func(root1.right,root2.right)
+
+BFs:
+  def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        def BFS(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            queue1 = collections.deque()
+            queue2 = collections.deque()
+            queue1.append(p)
+            queue2.append(q)
+            while queue1 and queue2:
+                node1 = queue1.popleft()
+                node2 = queue2.popleft()
+                # 如果都是空节点，跳过
+                if not node1 and not node2:
+                    continue 
+                # 如果有一个为空，不可能相等，返回false
+                if not node1 and node2 or not node2 and node1:
+                    return False
+                # 如果不为空，一定有值，若不相等，直接返回false。若相等，再后续添加节点
+                if node1.val != node2.val:
+                    return False
+                queue1.append(node1.left)
+                queue1.append(node1.right)
+                queue2.append(node2.left)
+                queue2.append(node2.right)
+            # 如果还有节点没遍历完，则一定不等
+            if queue1 or queue2:
+                return False
+            return True
+        return BFS(p, q)
+
+
+  
+  
+
+对称树
+dfs 
+def func(r1,r2):
+  
 4 平衡树（从上到下 从下到上）
 def func(root):
   def h(root):
