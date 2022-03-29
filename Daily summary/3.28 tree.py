a5b8df59e9cc763490e1159d1b5e587987d61a46
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-03-30 04:47:29 +0800

    Update 3.28 tree.py

--- Daily summary/3.28 tree.py
+++ Daily summary/3.28 tree.py
@@ -13,6 +13,16 @@ def func(root):
 中：
 def func(root):
   stack,res=[],[]
+  
+  def func(root):
+    
+    def dfs(root):
+      if not root:
+        return 0
+      left=dfs(root.left)
+      right=dfs(root.right)
+      left=left+1 if root.val=root.left.val else0
+      
   while root or stack:
     while root:
       stack.append(root)
@@ -23,6 +33,28 @@ def func(root):
     
 后序：
 与中序正好left,right相反 但是最后返回res[::-1]
+
+2 非自顶而下：
+相同节点值的最大路径
+def func(root):
+  self,ans=0
+  def dfs(root):
+    if not root:
+      return 0
+    left=dfs(root.left)
+    right=dfs(root.right)
+    if root.left:
+      left=left+1 if root.val==root.left.val else 0
+    if root.right:
+      right=right+1 if root.val==root.right.val else 0
+    self.ans=max(self.ans,left+right)
+    return max(left,right)
+  dfs(root)
+  
+  
+3 
+    
+    
 
   
     
