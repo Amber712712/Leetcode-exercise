881592c7fbe3a558b7c3e1093070a1b1585a5e52
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-07-15 19:12:50 +0800

    Update 7.15 排序.py

--- NG practise/7.15 排序.py
+++ NG practise/7.15 排序.py
@@ -1,1 +1,20 @@
 
+1 大根堆/小根堆
+数组中第K大的元素
+def func(nums):
+  heap=[]
+  for num in nums:
+    heapq.heappush(heap,num)
+    if len(heap)>k:
+      heapq.heappop(heap)
+  return heap[0]
+
+
+def func(nums,k):
+  heap=nums
+  heapq.heapify(heap)
+  n=len(nums)
+  for i in range(n-k):
+    heapq.heappop(heap)
+  return heap[0]
+
