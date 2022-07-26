bb3b1fa4d0e1ff6ec1cfc06d45253b24ff1783cb
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-07-27 07:06:59 +0800

    Update 7.25滑动窗口.py

--- NG practise/7.25滑动窗口.py
+++ NG practise/7.25滑动窗口.py
@@ -1,4 +1,6 @@
-1 
+1 最短子序列和大于等于定
+start end 指针 
+首先移动end指针
 def func(nums,target):
   start,end=0,0
   sum=0
@@ -19,6 +21,9 @@ def func(s):
     return 0
   n=len(s)
   lookup=set()
+  
+  
+  
   left=0
   curr_len=ans=0
   for i in range(n):
@@ -31,6 +36,82 @@ def func(s):
     lookup.add(s[i])
   return ans
 
+3 
+def func(s,target):
+  if len(s)<len(target):
+    return ""
+  dic=collections.Counter(target)
+  need=len(target)
+  start,end=0,0
+  min_len=len(s)+1
+  ans1=0
+  ans2=-1
+  while end<len(s):
+    if s[end] in dic:
+      if dic[s[end]]>0:
+        need-=1
+      dic[s[end]]-=1
+    while need==0:
+      if end-start+1<min_len:
+        ans2=end
+        ans1=start
+        
+      if s[start] in dic:
+        if dic[s[start]]>=0:
+          need+=1
+        dic[s[start]]+=1
+        start+=1
+    end+=1
+  return s[ans1:ans2+1]
+    
+  
+4 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false
 
+换句话说，s1 的排列之一是 s2 的 子串
+要求滑动窗口是 固定长度 
+def func(s1,t):
+  if len(s1)<len(t):
+    return []
+  need=len(t)
+  dic=collections.Counter(t)
+  start,end=0,0
+  n=len(s1)
+  res=[]
+  while end < n:
+    if s1[end] in dic:
+      if dic[s1[end]]>0:
+        need-=1
+      dic[s1[end]]-=1
+      
+    left=end-len(t)
+    if left>0:
+      if s1[left] in dic:
+        if dic[s1[left]]>=0:
+          need+=1
+        dic[s1[left]]+=1
+    if need==0:
+      res.append(left)
+        
+       
+    
+      
+        
+    
+ 
+  
+  
+
+
+      
+   
+    
+        
+        
+      
+      
+        
+    
+    
+  
 
 
