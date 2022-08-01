545b4fa18de8ee1148661bf1b8e335a04721b216
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-08-02 03:06:19 +0800

    Update new.py

--- NG practise/new.py
+++ NG practise/new.py
@@ -66,7 +66,101 @@ def func(nums):
   return ans
 
 
-5 
+5
+def func(s):
+  if not s:
+    return 0
+  n=len(s)
+  lookup=set()
+  start,end=0,0
+  min_len=n+1
+  
+  while end<n:
+   
+    while s[end] in lookup:
+      lookup.remove(s[start])
+      start+=1
+    min_len=min(min_len,end-start+1)
+    lookup.add(s[end])
+    
+ 
+6 假设你有两个数组，一个长一个短，短的元素均不相同。找到长数组中包含短数组所有的元素的最短子数组，其出现顺序无关紧要。
+def func(big,small):
+  if len(big)<len(small):
+    return []
+  need=len(small)
+  left,right=0,0
+  n=len(big)
+  resleft=0
+  dic=collections.Counter(small)
+  
+  while right<n:
+    if big[right] in dic:
+      if dic[big[right]]>0:
+        need-=1
+      dic[big[right]]-=1
+      
+    while need==0:
+      if left>resleft:
+        resleft=left
+        
+      if big[left] in dic:
+        if dic[big[left]]>=0:
+          need+=1
+        dic[big[left]]+=1
+      left+=1
+    end+=1
+  return resleft
+
+7 
+pre=[0]*n
+sum_=0
+for i in range(n):
+  if s[i]=="*":
+    sum_+=1
+  else:
+    pre[i]=sum_
+
+右边：
+right=-1
+r=[0]*n
+for i in range(n-1,-1,-1):
+  if s[i]=="|":
+    right=i
+  r[i]=right
+  
+  
+8 
+def func(s):
+  
+  while i<=j:
+    if s[i] not in s:
+      i+=1
+    if s[j] not in s:
+      j-=1
+    else:
+      
+      
+
+
+
+  
+  
+  
+  
+
+  
+    
+      
+      
+      
+  
+  
+ 
+  
+  
+  
+  
     
   
   
