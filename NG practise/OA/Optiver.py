db2acd8df6d004f3ce571da4a61dd5c81eb1e4f6
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-08-02 10:48:42 +0800

    Update Optiver.py

--- NG practise/OA/Optiver.py
+++ NG practise/OA/Optiver.py
@@ -28,6 +28,59 @@ def func(grid,target):
       if bfs(grid,row,col,t):
         print("pattern found at " +str(row) + ', ' + str(col))
         
+ 2 
+261 树是否有环 利用dfs+visited 从0到n-1
+如果边数>顶点数-1，肯定有环，如果小于，肯定有不连通
+
+s="(A,B)(B,C)(C,D)"
+def func(s):
+  if s[0] or s[-1]==" ":
+    return E1
+  
+def func(n,lists):
+  if len(lists)!=n-1:
+    return False
+  dic=defaultdict(list)
+  for x,y in lists:
+    dic[x].append(y)
+    dic[y].append(x)
+
+  visited=[False for _ in range(n)]
+  BFS：
+  Q=collections.deque()
+  Q.append(0)
+  while Q:
+    x=Q.popleft()
+    visited[x]=True
+    for y in dic[x]:
+      if visited[y]==False
+      Q.append(y)
+      
+  
+  #DFS：
+  def dfs(x):
+    visited[x]=True
+    for i in dic[x]:
+      if visited[i]=False:
+        dfs(i)
+      
+  for n in range(n):
+    if visited[n]==False:
+      return False
+  return True
+
+        
+      
+
+
+      
+
+  
+  
+
+
+
+        
 
   
   
