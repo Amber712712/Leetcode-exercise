52e2c4ae5d3eec2dc249ed8c7dbdf6be286e8afe
Zixuan(Amber) Chai <89145146+Amber712712@users.noreply.github.com>
2022-08-20 09:34:59 +0800

    Create Citadel.py

---
+++ NG practise/OA/Citadel.py
@@ -0,0 +1,29 @@
+1 三角形 do they belong
+def func(x1,y1,x2,y2,x3,y3,x,y):
+  l1=sqrt((x1-x2)**2+(y1-y2)**2)
+  l2=
+  
+  def area(x1, y1, x2, y2, x3, y3):
+ 
+    return abs((x1 * (y2 - y3) + x2 * (y3 - y1)
+                + x3 * (y1 - y2)) / 2.0)
+查看三角形面积之和是否一样
+
+2 points里的每个整数必须+k或-k，并返回新的points里最大值和最小值之差的最小可能值
+def func(points):
+  points.sort()
+  for i in range(len(points)):
+    min1=points[0]+k
+    max1=points[i]+k
+    min2=points[i+1]-k
+    max2=points[len(points)-1]-k
+    
+    
+3 m次方相乘得到一个值，把每个s[i]的该值相加得到一个和，返回这个和是奇数还是偶数
+
+    
+    
+    
+      
+    
+  
